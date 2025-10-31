8e501777d9f72dc27b6019ceb74d1576a2ed514b
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-10-31 15:58:36 -0300

    feat: updated Enemy entity

--- Entities/Enemy.cs
+++ Entities/Enemy.cs
@@ -1,7 +1,8 @@
 namespace OOP5.Entities {
 	public class Enemy : Character
 	{
-		public Enemy(string name, int baseLevel, int baseHealthPoints, float baseAttack)
-			: base(name, baseLevel, baseHealthPoints, baseAttack) {}
+		public Enemy(string name, int baseLevel, int baseHealthPoints,
+					float baseAttack, float stealChance)
+					: base(name, baseLevel, baseHealthPoints, baseAttack, stealChance) {}
 	}
 }
