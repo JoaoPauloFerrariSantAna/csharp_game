7b1f0b141bdeb23e4d10417e866425ca07b0a794
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-11-04 16:36:22 -0300

    feat: added constants

---
+++ Constants/LevelDefaults.cs
@@ -0,0 +1,10 @@
+namespace OOP5.Constants
+{
+	// because i need to check if HP has gotten negative
+	public enum LevelDefault 
+	{
+		CurrentLevel = 1,
+		Hitpoint = 20,
+		Attack = 5,
+	}
+}
---
+++ Constants/PossibleActions.cs
@@ -0,0 +1,9 @@
+namespace OOP5.Constants
+{
+	// because i need to check if HP has gotten negative
+	public enum PossibleAtions
+	{
+		Attack = 'a',
+		Defend = 'd'
+	}
+}
