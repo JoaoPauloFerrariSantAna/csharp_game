09d514748c64de9389315a186ae6e9132f8ecf68
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-11-04 20:37:17 -0300

    feat: added some simple rng

---
+++ Dices/DamageDice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OOP5.Dices
+{
+    public static class DamageDice
+    {
+        public static int CalculateReceivedDamage(int maxDamage, int minDamage)
+        {
+            Random receivedDamage = new Random();
+
+            return receivedDamage.Next(minDamage, maxDamage);
+        }
+    }
+}
---
+++ Dices/TargetDice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OOP5.Dices
+{
+    public static class TargetDice
+    {
+        public static int CalculateTargetIndex(int maxTargets, int minTarget = 1)
+        {
+            Random receivedDamage = new Random();
+
+            return receivedDamage.Next(minTarget, maxTargets);
+        }
+    }
+}
