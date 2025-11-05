f780fc381639ba955c6758dd03d72d935486a659
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-11-05 16:49:34 -0300

    feat: added method to Enemy class

--- Entities/Enemy.cs
+++ Entities/Enemy.cs
@@ -1,8 +1,22 @@
+using OOP5.Constants;
+using OOP5.Dices;
+using OOP5.Entities;
+
 namespace OOP5.Entities {
 	public class Enemy : Character
 	{
 		public Enemy(string name, int baseLevel, int baseHealthPoints,
 					float baseAttack)
 					: base(name, baseLevel, baseHealthPoints, baseAttack) {}
+
+		override public void Act(Character playerCharacter)
+		{
+			EnemyState state = (EnemyState)ActionDice.ChooseAction();
+
+			if(state == EnemyState.Attacking)
+			{
+				Attack(playerCharacter);
+			}
+		}
 	}
 }
