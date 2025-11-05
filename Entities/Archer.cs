d07cbaf5719fff1a1d26bb822416573c97b19299
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2025-11-05 16:02:30 -0300

    feat: added code to Archer

---
+++ Entities/Archer.cs
@@ -0,0 +1,7 @@
+namespace OOP5.Entities {
+	public class Archer : Character
+	{
+		public Archer(string name, int baseLevel, int baseHealthPoints, float baseAttack)
+			: base(name, baseLevel, baseHealthPoints, baseAttack) {}
+	}
+}
